9e681e4043ae4600c3f653d72388a2b13e1470fd
thedomino1313 <dominic.c.beyer@gmail.com>
2023-10-31 10:17:52 -0400

    starting game

--- model/game.py
+++ model/game.py
@@ -7,5 +7,10 @@ class BlackjackGame:
         self.id = 3
         self.players = players
         print(self.players)
+
+    def collect_bets(self):
+        # collect bets from each player with some sort of member function
+        # should we store these bets locally so that players cannot manipulate during the game?
+        pass
 # dq = agent.DeepQAgent()
 # p = agent.ProbAgent()
