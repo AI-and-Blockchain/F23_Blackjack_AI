59d6a234a0eda003bcc0122a6259c78fdca6912a
Angelica <alstar555@gmail.com>
2023-11-10 11:16:14 -0500

    help

--- frontend/blackjack_script.js
+++ frontend/blackjack_script.js
@@ -21,16 +21,15 @@ ctx.background = backgroundimage;
 
 //card vars
 var cardsize = [80, 120];
-cardList = [];
-cardCount = 0;
-cardSuits = ["spades", "hearts", "clubs", "diamonds"];
-numSuits = 4;
-cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
-numValues = 13;
-decklocation = [1000, 250];
-userlocation = [810, 610];
-card_backside = "/assets/Cards/backside.png";
-num_cards_dealt = 0;
+var cardSuits = ["spades", "hearts", "clubs", "diamonds"];
+var numSuits = 4;
+var cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
+var numValues = 13;
+console.log(width, height)
+var decklocation = [10/16 * width, 230/735*height];
+var userlocation = [71/160*width, 560/735*height];
+var cardoffset = [50, 10];
+var card_backside = "/assets/Cards/backside.png";
 
 deal_active = false;
 bet_active = true;
@@ -72,7 +71,7 @@ function paintcard(card){
   var cardSuit = card.suit
   var cardValue = card.value
   cardimg.src = `/assets/Cards/${cardValue}_of_${cardSuit}.png`;
-  if(card.sideup==-1){
+  if(card.sideup ===-1){
     cardimg.src = card_backside;
   }
   cardimg.onload = function (e)
@@ -96,14 +95,15 @@ function init_dealer_deck(){
 
 function deal_user_card(){
   ctx.beginPath();
-  var id = 0;
-  var x=userlocation[0] + (30*num_cards_dealt);
-  var y=userlocation[1] + (10*num_cards_dealt);
+  var x=userlocation[0] + (cardoffset[0]*cardCount);
+  var y=userlocation[1] + (cardoffset[1]*cardCount);
   var cardSuit = cardSuits[Math.ceil(Math.random()*100)%numSuits];
   var cardValue = cardValues[Math.ceil(Math.random()*100)%numValues];
   const card = new Card(x, y, id, cardSuit, cardValue);
+  card.id = id;
   id+=1;
   cardList.push(card);
+  console.log("user id", card.id)
   cardCount +=1;
   paintcard(card);
   num_cards_dealt += 1;
@@ -111,15 +111,22 @@ function deal_user_card(){
 
 function deal_dealer_card(){
   ctx.beginPath();
-  var id = 0;
-  var x=decklocation[0]-100;
-  var y=decklocation[1];
+  var x=decklocation[0]-(cardoffset[0]*numDealerCards)-200;
+  var y=decklocation[1]- (cardoffset[1]*numDealerCards);
   var cardSuit = cardSuits[Math.ceil(Math.random()*100)%numSuits];
   var cardValue = cardValues[Math.ceil(Math.random()*100)%numValues];
-  const card = new Card(x, y, id, cardSuit, cardValue);
+  var card = new Card(x, y, id, cardSuit, cardValue);
+  if(numDealerCards === 0){
+    card.fliplocked = 1;
+    card.sideup = -1; 
+  }else{
+    card.sideup = 1;
+  }
+  card.id = id;
   id+=1;
   cardList.push(card);
-  cardCount +=1;
+  console.log("dealer id", card.id)
+  cardCount;
   paintcard(card);
 }
 
@@ -142,11 +149,55 @@ function hit() {
 
 
 function stand() {
-  console.log("stand")
-  
+  if(stand_active){
+    
+    for (let i = 0; i < numPlayerCards-2; i++) {
+      setTimeout(function(){deal_dealer_card()}, 200*i);
+    }
+    var j=0;
+    for (let item of cardList) {
+      console.log(item.id);
+      item.sideup = 1;
+      item.fliplocked = 1;
+      setTimeout(function(){paintcard(item)}, 200*j);
+      j+=1
+    }
+    stand_active = false;
+    hit_active = false;
+    bet_active = true;
+    bet_amount = 0;
+  }
+}
+
+function new_game(){
+  numPlayerCards = 0;
+  bet_amount = 0;
+  numDealerCards = 0;
+  cardList = [];
+  cardCount = 0;
+  hit_active = false;
+  stand_active = false;
+  bet_active = true;
+  deal_active = false;
+  repaint_canvas();
+}
+
+function repaint_canvas(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  init_dealer_deck();
 }
 
 function bet (){
-  console.log("bet")
+  if(bet_active){
+    if(bet_amount===0){
+      new_game();
+    }
+    repaint_canvas();
+    deal_active = true;
+    ctx.fillStyle = "gold";
+    ctx.font = "20px Comic Sans";
+    bet_amount += 10;
+    ctx.fillText(`+${bet_amount} ETHER...`, 12/16*width, 270/735*height);
+  }
 }
 
