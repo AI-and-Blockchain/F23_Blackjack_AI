119e9fac2baae70420925717a6a61e77b594e808
thedomino1313 <dominic.c.beyer@gmail.com>
2023-11-21 23:36:29 -0500

    blockchain testing

--- blockchain/BlackjackBettingContract.sol
+++ blockchain/BlackjackBettingContract.sol
@@ -1,3 +1,6 @@
+/*
+/ guys this is so scuffed i just took the code from our lab and we need to modify it
+*/
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
@@ -9,10 +12,16 @@ contract BlackjackBettingContract {
 
     address payable owner;
 
-     event Received(address, uint);
+    event Received(address, uint);
+    mapping (address => uint256) private users;
 
     constructor() payable{
         owner = payable(msg.sender); 
+    }
+
+    // 
+    receive() external payable {
+        users[msg.sender] += msg.value;
     }
 
     function payWinner(address payable _to, uint256 _value) external payable returns (bool) 
@@ -26,5 +35,57 @@ contract BlackjackBettingContract {
     function receivePayment() external payable
     {
        emit Received(msg.sender, msg.value);
+    }
+
+    event DepositMade(address _from, address _to, uint _amount);
+    event WithdrawMade(address _from, address _to, uint _amount);
+
+    // modifier to check if caller has enough money
+    modifier canWithdraw(uint256 amount) {
+        require(amount < users[msg.sender]);
+        _;
+    }
+
+
+    /**
+     * @dev Deposits amount into the users account
+     */
+    function deposit() external payable {
+        emit DepositMade(msg.sender, address(this), msg.value);
+        users[msg.sender] += msg.value;
+    }
+
+    /**
+     * @dev Returns the user's balance
+     */
+    function getBalance(address user) public view returns (uint256) {
+        return users[user];
+    }
+
+    /**
+     * @dev Returns the total money stored in the contract
+     */
+    function contractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    /**
+     * @dev Withdraws money from the user's account if they have enough
+     */
+    function withdraw(uint256 amount) public payable canWithdraw(amount) {
+        emit WithdrawMade(address(this), msg.sender, amount);
+        (bool sent, ) = payable(msg.sender).call{value: amount}("");
+        users[msg.sender] -= amount;
+        require(sent, "Failed to withdraw");
+    }
+
+    /**
+     * @dev Withdraws all money from a user's account
+     */
+    function withdrawAll() public payable {
+        emit WithdrawMade(address(this), msg.sender, users[msg.sender]);
+        (bool sent, ) = payable(msg.sender).call{value: users[msg.sender]}("");
+        users[msg.sender] = 0;
+        require(sent, "Failed to withdraw");
     }
 } 
---
+++ blockchain/blockchain_test.py
@@ -0,0 +1,47 @@
+import json
+from web3 import Web3, exceptions
+
+infura_url = "https://sepolia.infura.io/v3/7b10c7ba73db47deac3f82c84bd00a94"
+
+import os  
+from dotenv import load_dotenv
+
+load_dotenv()  
+  
+private_key = input("Private Key: ").strip()
+from_account = "0x5233862f7245CB0d76af46716631abFB389163C0"
+to_account = "0xf9a568f094FEb5cfD453F1e8e13dfdbe55323B77"  
+web3 = Web3(Web3.HTTPProvider(infura_url))  
+  
+try:
+    from_account = web3.to_checksum_address(from_account)  
+except exceptions.InvalidAddress:  
+    print(f"Invalid 'from_account' address: {from_account}")  
+  
+try:  
+    to_account = web3.to_checksum_address(to_account)  
+except exceptions.InvalidAddress:  
+    print(f"Invalid 'to_account' address: {to_account}")  
+  
+nonce = web3.eth.get_transaction_count(from_account)  
+tx = {
+    'type': '0x2',
+    'nonce': nonce,
+    'from': from_account,
+    'to': to_account,
+    'value': web3.to_wei(0.00001, 'ether'),
+    'maxFeePerGas': web3.to_wei('250', 'gwei'),
+    'maxPriorityFeePerGas': web3.to_wei('3', 'gwei'),
+    'chainId': 11155111
+}
+gas = web3.eth.estimate_gas(tx)
+tx['gas'] = gas
+signed_tx = web3.eth.account.sign_transaction(tx, private_key)
+tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+print("Transaction hash: " + str(web3.to_hex(tx_hash)))
+
+
+abi = json.load(open("abi.json"))
+address = "0xf9a568f094FEb5cfD453F1e8e13dfdbe55323B77"
+contract = web3.eth.contract(abi=abi, address=address)
+print(contract.functions.getBalance("0x5233862f7245CB0d76af46716631abFB389163C0").call())
