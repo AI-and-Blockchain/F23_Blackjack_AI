ecfacebc507fdf2c2971787244da9ec3984cd589
thedomino1313 <dominic.c.beyer@gmail.com>
2023-11-22 16:03:41 -0500

    holy moly blockchain integration

--- frontend/static/login_script.js
+++ frontend/static/login_script.js
@@ -1,11 +1,7 @@
-function login() {
-    const dataForm = document.getElementById("dataForm");
-    const formData = new FormData(dataForm);
-    const jsonData = {};
-
-    formData.forEach((value, key) => {
-        jsonData[key] = value;
-    });
+async function login() {
+    await getAccount();
+    
+    const jsonData = {name: document.getElementById("name").value, address: account};
 
     fetch('/login', {
         method: 'POST',
@@ -16,17 +12,9 @@ function login() {
     })
     .then(response => response.json())
     .then(data => {
-        var x = 0;
         if (data.name == "invalid") {
             alert("Please enter a name");
-            x = 1;
-        }
-        if (data.address == "invalid") {
-            alert("Please enter a valid wallet address");
-            x = 1;
-        }
-        if (x == 0) {
-            document.getElementById("login_doc").innerHTML = "logging in...";
+        } else {
             location.href = "Blackjack.html";
         }
     })
@@ -34,22 +22,37 @@ function login() {
         console.error('Error:', error);
     });
   }
-
-  const ethereumButton = document.querySelector('.enableEthereumButton');
-  const sendEthButton = document.querySelector('.sendEthButton');
   
-  let accounts = ["0x5233862f7245CB0d76af46716631abFB389163C0"];
+  async function checkBalance() {
+    await getAccount();
+    
+    fetch('/getBalance', {
+        method: 'POST',
+        body: JSON.stringify({address: account, balance: 0}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("balanceLabel").innerHTML = data.balance;
+    })
+    
+  }
+
+  const deposit = document.querySelector('.deposit');
   
   // Send Ethereum to an address
-  sendEthButton.addEventListener('click', () => {
+  deposit.addEventListener('click', async() => {
+    await getAccount();
     ethereum
       .request({
         method: 'eth_sendTransaction',
         // The following sends an EIP-1559 transaction. Legacy transactions are also supported.
         params: [
           {
-            from: accounts[0], // The user's active address.
-            to: "0xf9a568f094FEb5cfD453F1e8e13dfdbe55323B77",
+            from: account, // The user's active address.
+            to: "0x4fd4b4Db50542818974f8Cac54F0fE384546ce94",
             value: '1000',
             gasLimit: '0x5028', // Customizable by the user during MetaMask confirmation.
             maxPriorityFeePerGas: '0x3b9aca00', // Customizable by the user during MetaMask confirmation.
@@ -61,10 +64,15 @@ function login() {
       .catch((error) => console.error(error));
   });
   
-  ethereumButton.addEventListener('click', () => {
-    getAccount();
-  });
-  
   async function getAccount() {
-    accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    await ethereum.request({ method: 'eth_requestAccounts' }).then(data => account = data[0]);
   }
+
+  window.onload = function() {
+    const MMSDK = new MetaMaskSDK.MetaMaskSDK()
+    // Because init process of the MetaMaskSDK is async.
+    setTimeout(() => {
+        const ethereum = MMSDK.getProvider() // You can also access via window.ethereum
+    }, 0)
+  }
+  
--- model/blockchain.py
+++ model/blockchain.py
@@ -1,5 +1,22 @@
-from typing import List
-from model.player import WebUser
+import json
+from web3 import Web3
+from dotenv import load_dotenv
 
-def place_bet(player: WebUser):
-    pass
+
+class BlockchainInterface():
+    def __init__(self, address):
+        self.infura_url = "https://sepolia.infura.io/v3/7b10c7ba73db47deac3f82c84bd00a94"
+
+        load_dotenv()  
+
+        self.web3 = Web3(Web3.HTTPProvider(self.infura_url))
+
+        abi = json.load(open("blockchain/abi.json"))
+        self.address = address
+        self.contract = self.web3.eth.contract(abi=abi, address=address)
+    
+    def getBalance(self, user):
+        return self.contract.functions.getBalance(Web3.to_checksum_address(user)).call()
+    
+    def payUser(self, user, amount):
+        return self.contract.functions.cashOut(Web3.to_checksum_address(user), amount).call()
